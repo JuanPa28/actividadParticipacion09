8acec0ba954b2e8b89b11d3048179f98fda22ae4
JuanPa28 <juanpramirez18@gmail.com>
2023-05-09 13:17:53 -0500

    Final commit

--- analizador.py
+++ analizador.py
@@ -1,13 +1,11 @@
 from typing import Dict, Any
 from collections import defaultdict
 
-
 class AnalizadorLogs:
     def __init__(self, nombre_archivo: str):
         self.nombre_archivo = nombre_archivo
 
     def procesar_logs(self) -> Dict[str, Any]:
-        # Diccionarios para llevar un registro de las estadísticas
         solicitudes_por_metodo = defaultdict(int)
         solicitudes_por_codigo = defaultdict(int)
         tamanio_total_respuesta = 0
@@ -26,20 +24,16 @@ class AnalizadorLogs:
                 codigo_respuesta = elementos[11]
                 tamanio_respuesta = int(elementos[12])
 
-                # Actualizamos las estadísticas
                 solicitudes_por_metodo[metodo_http] += 1
                 solicitudes_por_codigo[codigo_respuesta] += 1
                 tamanio_total_respuesta += tamanio_respuesta
                 num_solicitudes += 1
                 urls_solicitadas[url] += 1
 
-        # Calculamos el tamaño promedio de respuesta
         tamanio_promedio_respuesta = tamanio_total_respuesta / num_solicitudes if num_solicitudes > 0 else 0
 
-        # Ordenamos las URLs más solicitadas y nos quedamos con las 10 primeras
         urls_mas_solicitadas = dict(sorted(urls_solicitadas.items(), key=lambda x: x[1], reverse=True)[:10])
 
-        # Creamos un diccionario con las estadísticas calculadas
         estadisticas = {
             "num_solicitudes": num_solicitudes,
             "solicitudes_por_metodo": dict(solicitudes_por_metodo),
